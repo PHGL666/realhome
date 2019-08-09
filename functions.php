e730f116826b8ccbf5a2aa851c9e91aa8aecd58f
Gotzsche <pierregotzsche@gmail.com>
2019-08-09 14:21:43 +0200

    ajout photos et update header et functions

--- functions.php
+++ functions.php
@@ -1,36 +1,38 @@
 <?php
 // Theme configuration
-function scratch_setup(){
-// Document title
-add_theme_support( 'title-tag' );
+function scratch_setup()
+{
+    // Document title
+    add_theme_support('title-tag');
 
-// Post thumbnails
-add_theme_support( 'post-thumbnails' );
+    // Post thumbnails
+    add_theme_support('post-thumbnails');
 
-// Navigations
-register_nav_menus(
-array(
-'primary' => __( 'Primary Menu', 'scratch' ),
-)
-);
+    // Navigations
+    register_nav_menus(
+        array(
+            'primary' => __('Primary Menu', 'scratch'),
+            'social' => __('Social Menu', 'scratch'),
+        )
+    );
 
-// Custom Image sizes
-add_image_size('thumb-510',510, 205, true);
+    // Custom Image sizes
+    add_image_size('thumb-510', 510, 205, true);
 }
 
-add_action( 'after_setup_theme', 'scratch_setup' );
+add_action('after_setup_theme', 'scratch_setup');
 
 // Styles & scripts
-    function scratch_scripts()
-    {
-        wp_enqueue_style( 'googlefont', '//fonts.googleapis.com/css?family=Raleway:500&display=swap' );
-        wp_enqueue_style( 'googlefont', '//fonts.googleapis.com/css?family=Raleway:500,700&display=swap' );
-        wp_enqueue_style( 'googlefont', '//fonts.googleapis.com/css?family=Playfair+Display&display=swap' );
-        wp_enqueue_style('main-style', get_template_directory_uri() . '/css/main.min.css');
+function scratch_scripts()
+{
+    wp_enqueue_style('googlefont', '//fonts.googleapis.com/css?family=Raleway:500&display=swap');
+    wp_enqueue_style('googlefont', '//fonts.googleapis.com/css?family=Raleway:500,700&display=swap');
+    wp_enqueue_style('googlefont', '//fonts.googleapis.com/css?family=Playfair+Display&display=swap');
+    wp_enqueue_style('main-style', get_template_directory_uri() . '/css/main.min.css');
 
-        wp_enqueue_script( 'jquery');
-        wp_enqueue_script('bootstrap', get_template_directory_uri() . '/node_modules/bootstrap/dist/js/bootstrap.min.js');
-        wp_enqueue_script('slick', get_template_directory_uri() . '/node_modules/slick-carousel/slick/slick.min.js');
-        wp_enqueue_script('main', get_template_directory_uri() . '/js/main.js');
-    }
-    add_action('wp_enqueue_scripts', 'scratch_scripts');
+    wp_enqueue_script('jquery');
+    wp_enqueue_script('bootstrap', get_template_directory_uri() . '/node_modules/bootstrap/dist/js/bootstrap.min.js');
+    wp_enqueue_script('main', get_template_directory_uri() . '/js/main.js');
+}
+
+add_action('wp_enqueue_scripts', 'scratch_scripts');
--- header.php
+++ header.php
@@ -10,12 +10,20 @@
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-    <meta charset="<?php bloginfo(); ?>" />
+    <meta charset="<?php bloginfo(); ?>"/>
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?php wp_head(); ?>
 </head>
 
-<body <?php body_class();?>>
+<body <?php body_class(); ?>>
 
 <div id="page" class="wrapper">
+
+    <nav>
+        <?php wp_nav_menu( array(
+            'theme_location' => 'primary',
+            'container_class' => 'ml-auto'
+            // 'depth' => 1,
+        )); ?>
+    </nav>
