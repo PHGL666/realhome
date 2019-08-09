8c782245344c5ce97986b3ef8614436a0fb48d6a
Gotzsche <pierregotzsche@gmail.com>
2019-08-09 15:10:10 +0200

    update du menu primary et social OK

--- functions.php
+++ functions.php
@@ -28,6 +28,7 @@ function scratch_scripts()
     wp_enqueue_style('googlefont', '//fonts.googleapis.com/css?family=Raleway:500&display=swap');
     wp_enqueue_style('googlefont', '//fonts.googleapis.com/css?family=Raleway:500,700&display=swap');
     wp_enqueue_style('googlefont', '//fonts.googleapis.com/css?family=Playfair+Display&display=swap');
+    wp_enqueue_style('forkawesome', 'https://cdn.jsdelivr.net/npm/fork-awesome@1.1.7/css/fork-awesome.min.css');
     wp_enqueue_style('main-style', get_template_directory_uri() . '/css/main.min.css');
 
     wp_enqueue_script('jquery');
@@ -36,3 +37,6 @@ function scratch_scripts()
 }
 
 add_action('wp_enqueue_scripts', 'scratch_scripts');
+
+// Register Custome Navigation Walker
+require_once get_template_directory() . '/class-wp-bootstrap-navwalker.php';
--- header.php
+++ header.php
@@ -20,10 +20,30 @@
 
 <div id="page" class="wrapper">
 
-    <nav>
-        <?php wp_nav_menu( array(
-            'theme_location' => 'primary',
-            'container_class' => 'ml-auto'
-            // 'depth' => 1,
-        )); ?>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="navbar-brand mr-auto"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><img
+                        src="<?= get_stylesheet_directory_uri() ?>/images/logo.svg"
+                        alt="<?php bloginfo('name'); ?>"></a></div>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <?php wp_nav_menu(array(
+                'theme_location' => 'primary',
+                'container' => 'div',
+                'container_class' => 'mx-auto',
+                'container_id' => 'navbarNav',
+                'menu_class' => 'navbar-nav ml-auto',
+                'fallback_cb' => 'WP_Bootstrap_Navwalker::fallback',
+                'walker' => new WP_Bootstrap_Navwalker(),
+                // 'depth' => 1,
+            )); ?>
+
+            <?php wp_nav_menu(array(
+                'theme_location' => 'social',
+            ));
+            ?>
+        </div>
     </nav>
